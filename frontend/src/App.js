6eafa8f16c57138dbf4e86181d9d24b68b8b643b
Venkatesh <venkyakkati0310@gmail.com>
2025-06-05 12:00:54 +0530

    added env file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   const calculateResult = async () => {
     try {
-      const response = await fetch('http://localhost:8000/calculate', {
+      const response = await fetch('https://online-calculator.onrender.com/calculate', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
